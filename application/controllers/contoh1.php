d2d5d15fd8794a660b96d367d054da845b769146
SiscaLolitaAmalia <siscalolita22@gmail.com>
2023-09-25 15:29:53 +0700

    Pesan commit Anda di sini

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Sisca Lolita Amalia <br>
+            NIM 19220382 <br>
+            Kelas 19.3A.26 <br>
+            Saya tinggal di Jakarta Utara <br>
+            Hobi Saya Berlari :) ";
+    }
+}
